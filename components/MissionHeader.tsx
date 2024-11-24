5f25cb753df3eb9e930acaa1a8121dcc89876892
Adam G <adamgarscadden@gmail.com>
2024-11-24 19:57:47 +0000

    space: heading size

--- components/MissionHeader.tsx
+++ components/MissionHeader.tsx
@@ -9,7 +9,7 @@ const MissionHeader = (): JSX.Element => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className="border-b p-5">
-        <h1 className="font-sans uppercase text-3xl md:text-5xl pl-3" data-testid="mission-title">
+        <h1 className="font-sans uppercase text-3xl pl-3" data-testid="mission-title">
           🚀 SpaceX Launches
         </h1>
       </header>
