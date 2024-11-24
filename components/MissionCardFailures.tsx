0736b862bd4f31dc49535cdf7d3d76e5e4047734
Adam G <adamgarscadden@gmail.com>
2024-11-24 20:07:58 +0000

    space: removed extra padding

--- components/MissionCardFailures.tsx
+++ components/MissionCardFailures.tsx
@@ -2,8 +2,8 @@ import { SpaceXIntCardFailures } from '../APIResponsesTypes';
 
 const MissionCardFailures = ({ failures }: SpaceXIntCardFailures): JSX.Element => {
   return (
-    <span className="flex flex-col mb-7" data-testid="mission-card-failures">
-      <span className="flex flex-col mb-7">
+    <span className="flex flex-col" data-testid="mission-card-failures">
+      <span className="flex flex-col mb-1">
         <h3 className="font-mono mr-1 font-semibold">Why did the mission fail?</h3>
         <h4 className="font-mono capitalize" data-testid="mission-card-failures-text">
           {failures}
