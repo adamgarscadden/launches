1c13a093f99606a63a02283b442ee4658d4680f7
Adam G <adamgarscadden@gmail.com>
2024-11-24 19:16:19 +0000

    space: tidying up the mession card meta

--- components/MissionCardMeta.tsx
+++ components/MissionCardMeta.tsx
@@ -1,20 +1,20 @@
 import { SpaceXIntCardMeta } from '../APIResponsesTypes';
 
-const MissionCardMeta = ({ title, text, multiples }: SpaceXIntCardMeta): JSX.Element => {
+const MissionCardMeta = ({ title = '', text = '', multiples = [] }: SpaceXIntCardMeta): JSX.Element => {
   return (
     <div className="flex flex-col mb-7" data-testid="mission-card-meta">
       {title && (
-        <h3 className="font-mono mr-1 font-semibold" data-testid="mission-card-meta-title" aria-label="Meta title">
+        <h3 className="font-mono mr-1 font-semibold" data-testid="mission-card-meta-title" id={`meta-title-${title}`}>
           {title}
         </h3>
       )}
-      {text && !multiples && (
+      {text && !multiples.length && (
         <p className="font-mono" data-testid="mission-card-meta-text">
           {text}
         </p>
       )}
-      {multiples && (
-        <ul className="font-mono" data-testid="mission-card-meta-multiples">
+      {multiples.length > 0 && (
+        <ul className="font-mono" data-testid="mission-card-meta-multiples" aria-labelledby={`meta-title-${title}`}>
           {multiples.map((single, index) => (
             <li key={`${single}-${index}`} className="block">
               {single}
